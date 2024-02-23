b6acc7721eb6b5b8cd02f7d6add9a90dca27a4c5
Nikita Peplov <npeplov@yandex.ru>
2024-02-23 11:58:35 +0300

    posts from array

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,39 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <div class="post" v-for="post in posts">
+      <div><strong>Название: </strong>{{ post.title }}</div>
+      <div><strong>Описание: </strong>{{ post.body }}</div>
+    </div>
+
+  </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      posts: [
+        {id:1, title: "Javascript", body: "Lorem, ipsum dolor sit amet consectetur!"},
+        {id:2, title: "Javascript 2", body: "Lorem, ipsum dolor sit amet consectetur!2"},
+        {id:3, title: "Javascript 3", body: "Lorem, ipsum dolor sit amet consectetur!3"},
+      ]
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
 }
 </style>
