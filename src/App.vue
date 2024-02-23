25471b43b91d2a5b8cf60cc901aa5b1365e8cb32
Nikita Peplov <npeplov@yandex.ru>
2024-02-23 14:03:29 +0300

    add emit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
 <template>
-  <div>
-    <form @submit.prevent>
-      <h4>Создание поста</h4>
-      <input
-        v-bind:value="title"
-        type="text"
-        class="input"
-        placeholder="Название"
-        @input="title = $event.target.value"
-      />
-      <input
-        v-bind:value="body"
-        type="text"
-        class="input"
-        placeholder="Описание"
-        @input="body = $event.target.value"
-      />
-      <button @click="createPost">Создать</button>
-    </form>
-    <div class="post" v-for="post in posts">
-      <div><strong>Название: </strong>{{ post.title }}</div>
-      <div><strong>Описание: </strong>{{ post.body }}</div>
-    </div>
+  <div class="app">
+    <PostForm @create="createPost"/>
+    <PostList :posts="posts"
+    />
   </div>
 </template>
 
 <script>
+import PostForm from "@/components/PostForm"
+import PostList from "@/components/PostList"
 export default {
+  components: {
+    PostForm,
+    PostList
+  },
   data() {
     return {
       posts: [
@@ -46,20 +33,12 @@ export default {
           body: "Lorem, ipsum dolor sit amet consectetur!3",
         },
       ],
-      title: "",
-      body: "",
     };
   },
   methods: {
-    createPost() {
-      const newPost = {
-        id: Date.now(),
-        title: this.title,
-        body: this.body,
-      };
-      this.posts.push(newPost);
-      (this.title = ""), (this.body = "");
-    },
+    createPost(post) {
+      this.posts.push(post)
+    }
   },
 };
 </script>
@@ -71,9 +50,5 @@ export default {
   box-sizing: border-box;
 }
 
-.post {
-  padding: 15px;
-  border: 2px solid teal;
-  margin-top: 15px;
-}
+
 </style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <form @submit.prevent>
+    <h4>Создание поста</h4>
+    <input
+      v-model="post.title"
+      type="text"
+      class="input"
+      placeholder="Название"
+    />
+    <input
+      v-model="post.body"
+      type="text"
+      class="input"
+      placeholder="Описание"
+    />
+    <button @click="createPost">Создать</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        title: "",
+        body: "",
+      },
+    };
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.$emit('create', this.post);
+      this.post = {
+        title: "",
+        body: "",
+      }
+    },
+  },
+};
+</script>
+
+<style scoped></style>
