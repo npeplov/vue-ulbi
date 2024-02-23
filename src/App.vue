a9a35d1dd41867a26b1902f5de618aea02f98a6b
Nikita Peplov <npeplov@yandex.ru>
2024-02-23 12:48:01 +0300

    add posts form

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <template>
   <div>
+    <form @submit.prevent>
+      <h4>Создание поста</h4>
+      <input
+        v-bind:value="title"
+        type="text"
+        class="input"
+        placeholder="Название"
+        @input="title = $event.target.value"
+      />
+      <input
+        v-bind:value="body"
+        type="text"
+        class="input"
+        placeholder="Описание"
+        @input="body = $event.target.value"
+      />
+      <button @click="createPost">Создать</button>
+    </form>
     <div class="post" v-for="post in posts">
       <div><strong>Название: </strong>{{ post.title }}</div>
       <div><strong>Описание: </strong>{{ post.body }}</div>
     </div>
-
   </div>
 </template>
 
@@ -13,15 +30,38 @@ export default {
   data() {
     return {
       posts: [
-        {id:1, title: "Javascript", body: "Lorem, ipsum dolor sit amet consectetur!"},
-        {id:2, title: "Javascript 2", body: "Lorem, ipsum dolor sit amet consectetur!2"},
-        {id:3, title: "Javascript 3", body: "Lorem, ipsum dolor sit amet consectetur!3"},
-      ]
-    }
+        {
+          id: 1,
+          title: "Javascript",
+          body: "Lorem, ipsum dolor sit amet consectetur!",
+        },
+        {
+          id: 2,
+          title: "Javascript 2",
+          body: "Lorem, ipsum dolor sit amet consectetur!2",
+        },
+        {
+          id: 3,
+          title: "Javascript 3",
+          body: "Lorem, ipsum dolor sit amet consectetur!3",
+        },
+      ],
+      title: "",
+      body: "",
+    };
   },
   methods: {
-  }
-}
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      };
+      this.posts.push(newPost);
+      (this.title = ""), (this.body = "");
+    },
+  },
+};
 </script>
 
 <style>
